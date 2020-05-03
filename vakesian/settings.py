b38bcc11814f1fb6484dd29e145bac1bb2d4c21c
stali1234 <stalin.sangeethraj@gmail.com>
2020-05-03 15:49:45 -0400

    *****

--- vakesian/settings.py
+++ vakesian/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import django_heroku
+ 
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -130,5 +130,5 @@ STATICFILES_DIRS=[
 STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
 
-django_heroku.settings(locals())
+ 
 
